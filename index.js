c271f8ebf259fe486c42c58374519572c0c1b63e
SaToGo1 <85353835+SaToGo1@users.noreply.github.com>
2023-06-21 15:54:37 +0000

    Add Post request to /api/filename, add fileupload middleware

--- index.js
+++ index.js
@@ -2,19 +2,25 @@ var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
 
+const fileUpload = require('express-fileupload');
+// const multer = require('multer')
+// const upload = multer() // { dest: 'uploads/' } if i need the file
+
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(fileUpload())
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse',  (req, res) => {
+  console.log(req.files);
+})
 
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
+app.listen(port, function() {
   console.log('Your app is listening on port ' + port)
 });
